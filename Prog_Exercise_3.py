44d8e3f104f87500b5e06585a7f45fe158543241
SioPauwii <paulocordova3@gmail.com>
2024-09-17 07:25:47 +0800

    for demo

--- Prog_Exercise_3.py
+++
@@ -1,7 +0,0 @@
-str1 = input('Enter your string to compare: ')
-str2 = input('Enter the string to compate to: ')
-
-if(str1 == str2):
-    print(f'"{str1}" is equal to "{str2}"')
-else:
-    print(f'"{str1}" is not equal to "{str2}"')
